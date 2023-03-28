e11b4227116daeed73cbceb9578f1491c9dd1595
Tiffany Lin <tl330@duke.edu.tw>
2023-03-27 20:53:28 -0400

    change settings.py

--- docker-deploy/web-app/firsthomework/settings.py
+++ docker-deploy/web-app/firsthomework/settings.py
@@ -81,10 +81,10 @@ WSGI_APPLICATION = 'firsthomework.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'uber_share',
-        'USER': 'tl330',
-        'PASSWORD': 'tl330',
-        'HOST': '127.0.0.1',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'db',
         'PORT': '5432',
     }
 }
